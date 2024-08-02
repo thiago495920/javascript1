b848af313367ff82fc9678b4a373708f45b30a50
thiago495920 <thiagoroquesoares100@gmail.com>
2024-08-02 16:45:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let nome = "thiago"
+let idade = 16
+let ativo = true
+
+console.log("Nome:", nome);         
+console.log("Idade:", idade);      
+console.log("Ativo:", ativo);       
+
+function somar(a, b) {
+return a + b;
+}
+console.log(somar(5,3));
+
+document.getElementById('meuBotao').addEventListener('click', function() {
+    alert('clicou!("aqui e o brasillll")');
+});
